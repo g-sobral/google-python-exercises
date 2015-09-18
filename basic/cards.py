962f83d8610b817526451c8e50e1cf1113b158e9
Gabriel Sobral <gasan.sobral@gmail.com>
2015-09-18 16:15:31 -0300

    Include cards and vector OO exercises

---
+++ basic/cards.py
@@ -0,0 +1,39 @@
+
+
+class Card:
+    def __init__(self, rank, suit):
+        self.rank = rank
+        self.suit = suit
+
+
+    def __eq__(self, other):
+        return (self.rank, self.suit) == (other.rank, other.suit)
+
+
+class Deck(list):
+    suits = ('spades', 'diamonds', 'clubs', 'hearts')
+    ranks = ('2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A')
+
+    def __init__(self):
+        for s in self.suits:
+            for r in self.ranks:
+                self.append(Card(rank=r, suit=s))
+
+
+def main():
+    deck = Deck()
+    assert deck
+    assert len(deck) == 52
+    assert Card(rank='2', suit='spades') == Card(rank='2', suit='spades')
+    assert deck[0] == Card(rank='2', suit='spades')
+    assert deck[:2] == [Card('2', 'spades'), Card('3', 'spades')]
+    assert deck[12::13] == [Card('A', 'spades'), Card('A', 'diamonds'),
+                            Card('A', 'clubs'), Card('A', 'hearts')]
+    assert Card('Q', 'hearts') in Deck()
+
+    deck[0] = None; assert deck[0] is None
+    deck[:2] = [None, None]; assert deck[:2] == [None, None]
+
+
+if __name__ == '__main__':
+    main()
---
+++ basic/vector.py
@@ -0,0 +1,61 @@
+from math import sqrt
+
+class Vector:
+
+    # __slots__ = ['x', 'y']
+
+    def __init__(self, x, y):
+        super().__setattr__('x', x)
+        super().__setattr__('y', y)
+
+    def __eq__(self, other):
+        return (self.x, self.y) == (other.x, other.y)
+
+    def __repr__(self):
+        return ''.join(('Vector(', str(self.x), ', ', str(self.y), ')'))
+
+    def __str__(self):
+        return ''.join(('(', str(self.x), ', ', str(self.y), ')'))
+
+    def __add__(self, other):
+        x = self.x + other.x
+        y = self.y + other.y
+        return Vector(x, y)
+
+    def __mul__(self, other):
+        x = self.x * other
+        y = self.y * other
+        return Vector(x, y)
+
+    def __abs__(self):
+        return sqrt(pow(self.x, 2) + pow(self.y, 2))
+
+    def __bool__(self):
+        if self.x != 0 or self.y != 0:
+            return True
+        else:
+            return False
+
+    def dot(self, other):
+        return self.x * other.x + self.y * other.y
+
+    def __setattr__(self, key, value):
+        msg = "'%s' does not support attribute assignment" % (self.__class__)
+        raise AttributeError(msg)
+
+def main():
+    assert Vector(2, 4)
+    assert (Vector(2, 4).x, Vector(2, 4).y) == (2, 4)
+    assert repr(Vector(2, 4)) == 'Vector(2, 4)'
+    assert str(Vector(2, 4)) == '(2, 4)'
+    assert Vector(2, 4) == Vector(2, 4)
+    assert Vector(2, 4) + Vector(1, 2) == Vector(3, 6)
+    assert Vector(2, 4) * 2 == Vector(4, 8)
+    assert abs(Vector(3, 4)) == 5.0
+    assert bool(Vector(0, 0)) == False
+    assert bool(Vector(0, 1)) == bool(Vector(1, 0)) == bool(Vector(1, 1))
+    assert Vector(2, 2).dot(Vector(3, 4)) == 14
+
+
+if __name__ == '__main__':
+    main()
